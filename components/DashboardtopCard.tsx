f9b32f5c9f075fd5ec4efe2a59f7d58b9d3c48b6
Aman-Agnihotri36 <agnihotriaman36@gmail.com>
2025-08-10 08:57:00 +0530

    remove

--- components/DashboardtopCard.tsx
+++ components/DashboardtopCard.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Card } from './ui/card'
-import LineChart from './LineChart'
+
 
 function DashboardtopCard({
     title = 'Price',
@@ -11,9 +11,7 @@ function DashboardtopCard({
             <div className='flex flex-col h-48'>
                 <div className='text-gray-600 text-sm font-medium mb-2'>{title}</div>
                 <div className='text-2xl font-bold text-gray-800 mb-4'>{value}</div>
-                <div className='flex-grow'>
-                    <LineChart />
-                </div>
+
             </div>
         </Card>
     )
